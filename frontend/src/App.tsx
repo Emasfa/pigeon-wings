27cba282a88a60fd00528b44f95ba09d6c1497a6
Etienne <etienne.masfaraud@gmail.com>
2025-10-15 15:45:34 +0200

    build send footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 import { formatTime } from "./utils/formatTime";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   const [messages, setMessages] = useState<Message[]>([]);
@@ -32,7 +33,7 @@ function App() {
   return (
     <>
       <Grid container spacing={2}>
-        <AppBar component="nav" sx={{ bgcolor: "#767676ff" }}>
+        <AppBar component="nav" sx={{ bgcolor: "#e1e1e1ff" }}>
           <Box display="flex" alignItems="center" gap={2} sx={{ padding: 0.5 }}>
             <Bird size={60} color="#62048eff" strokeWidth={1.5} />
             <Typography
@@ -54,10 +55,10 @@ function App() {
             </MessageBox>
           ))}
 
-          <MessageBox timestamp={"00:00"}>That's a static message.</MessageBox>
+          <MessageBox timestamp={"00:00"}>This is a static message.</MessageBox>
         </Grid>
         <Grid size={12}>
-          <Box sx={{ bgcolor: "#767676ff" }}> Footer</Box>
+          <Footer></Footer>
         </Grid>
       </Grid>
     </>
---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,50 @@
+import { IconButton, Paper, TextField } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+
+const Footer = () => {
+  return (
+    <Paper
+      elevation={3}
+      sx={{
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        right: 0,
+        p: 3,
+        display: "flex",
+        gap: 1,
+        bgcolor: "#e1e1e1ff",
+        borderRadius: 0,
+      }}
+    >
+      <TextField
+        fullWidth
+        variant="outlined"
+        placeholder="Type your message..."
+        size="medium"
+        sx={{
+          bgcolor: "#dbd9d9ff", // background color
+          borderRadius: 2, // outer border radius
+          "& .MuiOutlinedInput-root": {
+            borderRadius: 2, // border radius for outline
+            "& fieldset": {
+              borderColor: "#62048eff", // normal border color
+            },
+            "&:hover fieldset": {
+              borderColor: "#62048eff", // border color on hover
+            },
+            "&.Mui-focused fieldset": {
+              borderColor: "#62048eff", // border color when focused
+              borderWidth: 2, // border width when focused
+            },
+          },
+        }}
+      />
+      <IconButton sx={{ color: "#62048eff" }} aria-label="send">
+        <SendIcon />
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default Footer;
