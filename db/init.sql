2a9cb30dad14b8cffffee245c0a22f24f0245344
Etienne <etienne.masfaraud@gmail.com>
2025-10-21 11:37:10 +0200

    Add send and receiver identities to messages tables

--- db/init.sql
+++ db/init.sql
@@ -10,14 +10,17 @@ CREATE TABLE IF NOT EXISTS messages (
     id SERIAL PRIMARY KEY,
     content TEXT NOT NULL,
     created_at TIMESTAMPTZ DEFAULT now(),
-    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+    from_user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    to_user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
 INSERT INTO users (username)
 VALUES ('Johnny B. Goode');
 
-INSERT INTO messages (content, user_id)
+INSERT INTO messages (content, from_user_id, to_user_id)
 VALUES 
-  ('Hello World !', 1),
-  ('This is a second message.', 1),
-  ('Lorem ipsum was initially extracted from Cicero''s original text, ''The Extremes of Good and Evil''. Interestingly, ''lorem'' is not an actual latin word but the ''dolorem'' word cut in half on original test print by Aldus Manutius during the 16th century.', 1);
+  ('Hello World !', 1, 2),
+  ('This is a second message.', 1, 2),
+  ('What are you blabbering about ?', 2, 1),
+  ('Lorem ipsum was initially extracted from Cicero''s original text, ''The Extremes of Good and Evil''. Interestingly, ''lorem'' is not an actual latin word but the ''dolorem'' word cut in half on original test print by Aldus Manutius during the 16th century.', 1, 2),
+  ('I''m calling the police.', 2, 1);
